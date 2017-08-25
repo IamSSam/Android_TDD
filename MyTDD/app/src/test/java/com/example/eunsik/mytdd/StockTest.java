7d4ff03996794b1521befcf7c00b140938252116
IamSSam <kongse92@gmail.com>
2017-08-25 15:26:48 +0900

    Make MainActivityText

--- MyTDD/app/src/test/java/com/example/eunsik/mytdd/StockTest.java
+++ MyTDD/app/src/test/java/com/example/eunsik/mytdd/StockTest.java
@@ -11,9 +11,9 @@ import org.junit.Test;
 public class StockTest {
     @Test
     public void storck_init_setFields() {
-        Stock stock = new Stock("0001", "삼성전자", 10000, 100, 10, true, false);
+        Stock stock = new Stock("00001", "삼성전자", 10000, 100, 10, true, false);
         Assert.assertNotNull(stock);
-        Assert.assertEquals("0001", stock.getStockCode());
+        Assert.assertEquals("00001", stock.getStockCode());
         Assert.assertEquals("삼성전자", stock.getName());
         Assert.assertEquals(10000, stock.getCurrentPrice(), 0.001);
         Assert.assertEquals(100, stock.getPriceDiff(), 0.001);
@@ -27,6 +27,6 @@ public class StockTest {
     public void stock_setQuantity_getValue() {
         Stock stock = new Stock("00001", "삼성전자", 10000, 100, 10, true, false);
         stock.setQuantity(10);
-        Assert.assertEquals(15000 * 10, stock.getValue(), 0.001);
+        Assert.assertEquals(10000 * 10, stock.getValue(), 0.001);
     }
 }
