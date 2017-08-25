baf6d11382100c093d40ffe8b2549a9f06688cde
IamSSam <kongse92@gmail.com>
2017-08-25 15:40:08 +0900

    Modify from MainActivity to MainFragment

--- MyTDD/app/src/main/java/com/example/eunsik/mytdd/MainActivity.java
+++ MyTDD/app/src/main/java/com/example/eunsik/mytdd/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.eunsik.mytdd;
 
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_fragment);
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+
+        if (fragment == null) {
+            fragment = createFragment();
+            fm.beginTransaction().add(R.id.fragmentContainer, fragment).commit();
+        }
     }
+
+    private Fragment createFragment() {
+        return new MainFragment();
+    }
+
 }
